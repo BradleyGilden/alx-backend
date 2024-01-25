d2eedbd7fb4f3c3782f18e0e1d87ab1cd980e875
BradleyGilden <bradleygilden@gmail.com>
2024-01-25 12:36:19 +0200

    changed tuple type

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,7 +7,7 @@ Date: 25-01-2024
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """returns index depending on page position and size"""
     return ((page - 1) * page_size, page * page_size)
 
