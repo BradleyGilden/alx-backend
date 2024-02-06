4fe480126e0a8dc11b6d6c1858220b0b8227165d
BradleyGilden <bradleygilden@gmail.com>
2024-02-06 13:23:08 +0200

    more templates

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -6,7 +6,7 @@ Forcing Locale
 Author: Bradley Dillion Gilden
 Date: 06-02-2024
 """
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
 import pytz
 
@@ -22,6 +22,23 @@ app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user(login_as):
+    """
+    get_user.
+    """
+    try:
+        return users.get(int(login_as))
+    except Exception:
+        return
+
 
 @babel.localeselector
 def get_locale():
@@ -34,10 +51,18 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+@app.before_request
+def before_request():
+    """
+    before_request
+    """
+    g.user = get_user(request.args.get("login_as"))
+
+
 @app.route("/", strict_slashes=False)
 def index():
     """returns the page index"""
-    return render_template("4-index.html")
+    return render_template("3-index.html")
 
 
 if __name__ == '__main__':
